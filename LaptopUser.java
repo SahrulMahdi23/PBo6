d01e4fe68b73580d2abf1fb97e2aab62b4d0ebf2
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-05 05:07:04 +0700

    Create LaptopUser.java

---
+++ LaptopUser.java
@@ -0,0 +1,27 @@
+package user;
+
+import laptop.Laptop;
+
+public class LaptopUser {
+    private Laptop laptop;
+
+    public LaptopUser(Laptop laptop) {
+        this.laptop = laptop;
+    }
+
+    public void turnOnLaptop() {
+        laptop.powerOn();
+    }
+
+    public void turnOffLaptop() {
+        laptop.powerOff();
+    }
+
+    public void makeLaptopLouder() {
+        laptop.volumeUp();
+    }
+
+    public void makeLaptopSilent() {
+        laptop.volumeDown();
+    }
+}
