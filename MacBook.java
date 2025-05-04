895a252cfdd1abbfa0e3e95c93ad06da53274060
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-05 05:04:57 +0700

    Create MacBook.java

---
+++ MacBook.java
@@ -0,0 +1,21 @@
+ package laptop;
+
+ public class MacBook implements Laptop {
+ 
+     public void powerOn() {
+         System.out.println("MacBook menyala dengan elegan.");
+     }
+ 
+     public void powerOff() {
+         System.out.println("MacBook dimatikan.");
+     }
+ 
+     public void volumeUp() {
+         System.out.println("Volume MacBook bertambah.");
+     }
+ 
+     public void volumeDown() {
+         System.out.println("Volume MacBook berkurang.");
+     }
+ }
+ 
