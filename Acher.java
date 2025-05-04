1e00343fd2ad283f3e76cfb20c67adbca7af1da1
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-05 05:04:03 +0700

    Create Acher.java

---
+++ Acher.java
@@ -0,0 +1,21 @@
+ package laptop;
+
+ public class Acher implements Laptop {
+ 
+     public void powerOn() {
+         System.out.println("Acher menyala...");
+     }
+ 
+     public void powerOff() {
+         System.out.println("Acher dimatikan...");
+     }
+ 
+     public void volumeUp() {
+         System.out.println("Acher Lenovo dinaikkan.");
+     }
+ 
+     public void volumeDown() {
+         System.out.println("Acher Lenovo diturunkan.");
+     }
+ }
+ 
