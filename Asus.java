43a9c43cdbdb521d17735c8f0e9e3d20e700a156
SahrulMahdi23 <147253671+SahrulMahdi23@users.noreply.github.com>
2025-05-05 05:06:05 +0700

    Create Asus.java

---
+++ Asus.java
@@ -0,0 +1,20 @@
+package laptop;
+
+ public class Asus implements Laptop {
+ 
+     public void powerOn() {
+         System.out.println("Asus hidup...");
+     }
+ 
+     public void powerOff() {
+         System.out.println("Asus mati...");
+     }
+ 
+     public void volumeUp() {
+         System.out.println("Asus Toshiba naik.");
+     }
+ 
+     public void volumeDown() {
+         System.out.println("Asus Toshiba turun.");
+     }
+ }
